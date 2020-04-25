38c1e46c8a2043eb597aa47b8caea64dfd353aa6
Buvan Prithivee <buvan.prith21@gmail.com>
2020-04-25 17:33:03 +0530

    Updated on 25, April

--- js/expandNews.js
+++ js/expandNews.js
@@ -1,9 +1,13 @@
-let newsData = JSON.parse(localStorage.getItem(sessionStorage.getItem('newsId')));
+document.body.onload = function(){
+    let newsData = JSON.parse(localStorage.getItem(sessionStorage.getItem('newsId')));
+    
+    document.getElementsByClassName('tile-title')[0].textContent = newsData[0];
+    document.getElementsByClassName('tile-description')[0].innerHTML = newsData[1];
+    if(newsData[2])
+        document.getElementsByClassName('footage')[0].src = newsData[2];
+    else
+        document.getElementsByClassName('footage')[0].style.display = 'none';
 
-document.getElementsByClassName('tile-title')[0].textContent = newsData[0];
-document.getElementsByClassName('tile-description')[0].innerHTML = newsData[1];
-if(newsData[2])
-    document.getElementsByClassName('footage')[0].src = newsData[2];
-else
-    document.getElementsByClassName('footage')[0].style.display = 'none';
+    
+}    
 
--- js/newsAddition.js
+++ js/newsAddition.js
@@ -1,56 +1,59 @@
-const newsContainer = document.getElementsByClassName('news-container')[1];
-for(let key of Object.keys(localStorage)){
-    let newNews = JSON.parse(localStorage.getItem(key));
-
-    let listEle = document.createElement('li');
-    let newsTile = document.createElement('div');
-    let newsExpand = document.createElement('div');
-    let tileContent = document.createElement('div');
-    let nextPage = document.createElement('a');
-    let insideTile = document.createElement('div');
-    let tileTitle = document.createElement('p');
-    let contentFrom = document.createElement('div');
-    let moreOptions = document.createElement('span');
-    listEle.setAttribute('class','news-tile-container');
-    newsTile.setAttribute('class','news-tile');
-    tileContent.setAttribute('class','tile-content');
-    nextPage.setAttribute('class','next-page');
-    nextPage.setAttribute('href','./viewNews.html');
-    nextPage.setAttribute('id', key);
-    insideTile.setAttribute('class','inside-tile');
-    tileTitle.setAttribute('class','tile-title');
-    contentFrom.setAttribute('class','content-from');
-    moreOptions.setAttribute('class','more-options');
-    newsExpand.setAttribute('class','news-expand');
-
-    nextPage.onclick = function(){
-        sessionStorage.setItem('newsId', this.id);
+document.body.onload = function() {
+    const newsContainer = document.getElementsByClassName('news-container')[1];
+    for(let key of Object.keys(localStorage)){
+        let newNews = JSON.parse(localStorage.getItem(key));
+    
+        let listEle = document.createElement('li');
+        let newsTile = document.createElement('div');
+        let newsExpand = document.createElement('div');
+        let tileContent = document.createElement('div');
+        let nextPage = document.createElement('a');
+        let insideTile = document.createElement('div');
+        let tileTitle = document.createElement('p');
+        let contentFrom = document.createElement('div');
+        let moreOptions = document.createElement('span');
+        listEle.setAttribute('class','news-tile-container');
+        newsTile.setAttribute('class','news-tile');
+        tileContent.setAttribute('class','tile-content');
+        nextPage.setAttribute('class','next-page');
+        nextPage.setAttribute('href','./viewNews.html');
+        nextPage.setAttribute('id', key);
+        insideTile.setAttribute('class','inside-tile');
+        tileTitle.setAttribute('class','tile-title');
+        contentFrom.setAttribute('class','content-from');
+        moreOptions.setAttribute('class','more-options');
+        newsExpand.setAttribute('class','news-expand');
+    
+        nextPage.onclick = function(){
+            sessionStorage.setItem('newsId', this.id);
+        }
+    
+        tileTitle.textContent= newNews[0];
+        let contentFromContent = `User Created News`;
+        let moreOptionsContent = `  <span class="popup-icon" aria-title="Save for later"><span class="bookmark-icon"></span></span>
+                                    <span class="popup-icon" aria-title="Share"><span class="share-icon"></span></span>
+                                    <span class="popup-icon" aria-title="More"><span class="more-icon"></span></span>`;
+    
+        let newsExpandContent = `<div class="newspaper-icon"><i></i></div>View full article<span>&#x2039;</span>`;
+        moreOptions.innerHTML = moreOptionsContent;
+        contentFrom.innerHTML = contentFromContent;
+        contentFrom.append(moreOptions);
+        newsExpand.innerHTML = newsExpandContent;
+        insideTile.appendChild(tileTitle);
+        insideTile.appendChild(contentFrom);
+    
+        tileContent.appendChild(nextPage);
+        tileContent.appendChild(insideTile);
+    
+        newsTile.append(tileContent);
+    
+        listEle.appendChild(newsTile);
+        listEle.appendChild(newsExpand);
+    
+        newsContainer.appendChild(listEle);
     }
-
-    tileTitle.textContent= newNews[0];
-    let contentFromContent = `User Created News`;
-    let moreOptionsContent = `  <span class="popup-icon" aria-title="Save for later"><span class="bookmark-icon"></span></span>
-                                <span class="popup-icon" aria-title="Share"><span class="share-icon"></span></span>
-                                <span class="popup-icon" aria-title="More"><span class="more-icon"></span></span>`;
-
-    let newsExpandContent = `<div class="newspaper-icon"><i></i></div>View full article<span>&#x2039;</span>`;
-    moreOptions.innerHTML = moreOptionsContent;
-    contentFrom.innerHTML = contentFromContent;
-    contentFrom.append(moreOptions);
-    newsExpand.innerHTML = newsExpandContent;
-    insideTile.appendChild(tileTitle);
-    insideTile.appendChild(contentFrom);
-
-    tileContent.appendChild(nextPage);
-    tileContent.appendChild(insideTile);
-
-    newsTile.append(tileContent);
-
-    listEle.appendChild(newsTile);
-    listEle.appendChild(newsExpand);
-
-    newsContainer.appendChild(listEle);
 }
+
 
 
 
